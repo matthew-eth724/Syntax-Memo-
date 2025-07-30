eeb58a901d18740f1650b8966c061d3a47e24df3
matthew-eth724 <tokoniayakazi.eth@gmail.com>
2025-07-30 11:05:38 +0100

    added Read, Update and Delete

---
+++ src/controllers/logic/user.ts
@@ -0,0 +1,35 @@
+import { iUser, User } from "../../models/user";
+import IUser from "../../types/user";
+import { Types } from "mongoose";
+
+export interface IUpdateUser {
+    user: IUser,
+    data: Object
+}
+
+export const getUser = async (id: Types.ObjectId): Promise<iUser> => {
+    try {
+        const user: iUser = await User.findById(id)
+        return user
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export const updateUser = async (data: IUpdateUser): Promise<iUser> => {
+    try {
+        const user: iUser = await User.findByIdAndUpdate(data.user.id, data.data)
+        const updatedUser: iUser = await User.findOne(data.data)
+        return updatedUser
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export const deleteUser = async (id: Types.ObjectId) => {
+    try {
+        await User.findByIdAndDelete(id)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
